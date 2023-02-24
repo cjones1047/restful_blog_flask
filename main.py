46930a752ac1e6fa5871532047237d0b54dccba6
cjones1047 <cjmj1047@gmail.com>
2023-02-24 07:29:36 -0600

    get_all_posts route retrieves all rows from database

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, exc
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
+import os
+
+file_path = os.path.abspath(os.getcwd())+"/posts.db"
 
 
 ## Delete this code:
@@ -17,12 +21,11 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
 db = SQLAlchemy(app)
 
 
-##CONFIGURE TABLE
+# CONFIGURE TABLE
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
@@ -33,7 +36,7 @@ class BlogPost(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
-##WTForm
+# WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
@@ -45,7 +48,9 @@ class CreatePostForm(FlaskForm):
 
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html", all_posts=posts)
+    all_posts = db.session.query(BlogPost).all()
+
+    return render_template("index.html", all_posts=all_posts)
 
 
 @app.route("/post/<int:index>")
